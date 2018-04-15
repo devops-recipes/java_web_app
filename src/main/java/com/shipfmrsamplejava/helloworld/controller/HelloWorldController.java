968dade57b9410596cf8c99005c05996d4bd473e
Avi Cavale <avi@shippable.com>
2018-04-14 17:46:02 -0700

    changing from shipfmrsamplejava to demo

--- src/main/java/com/shipfmrsamplejava/helloworld/controller/HelloWorldController.java
+++ src/main/java/com/shipfmrsamplejava/helloworld/controller/HelloWorldController.java
@@ -1,4 +1,4 @@
-package com.shipfmrsamplejava.helloworld.controller;
+package com.demo.helloworld.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
